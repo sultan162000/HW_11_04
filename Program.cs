ae26bf46413d2ae618783821b086a2babfc8e88a
Кодиров Султон <sultonkodiro109@gmail.com>
2020-04-13 20:41:21 +0500

    Added class book

--- Program.cs
+++ Program.cs
@@ -10,5 +10,24 @@ namespace HW_11_04
         }
     }
 
+    class Book{
+        public string Author{get;set;}
+        public string nameBook{get;set;}
+
+        public Book(string Author, string nameBook){
+            this.Author = Author;
+            this.nameBook = nameBook;
+
+        }
+        
+
+        public void showBooks(){
+            System.Console.WriteLine("Author: " +Author);
+            System.Console.WriteLine("Book: "+nameBook);
+
+        }
+    }
+
+
     
 }
