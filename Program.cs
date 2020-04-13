054757bc9dae145f61f80578445d3dfd50e83aef
Кодиров Султон <sultonkodiro109@gmail.com>
2020-04-13 20:48:22 +0500

    Finish project

--- Program.cs
+++ Program.cs
@@ -6,6 +6,14 @@ namespace HW_11_04
     {
         static void Main(string[] args)
         {
+            Book[] books = new Book[]{
+                new Book(Author : "Рей Брэдбери", nameBook : "451° по Фаренгейту"),
+                new Book(Author : "Габриэль Гарсиа", nameBook : "Сто лет одиночества"),
+                new Book(Author : "Джоан К. Роулинг", nameBook : "Гарри Поттер и философский камень"),
+                new Book(Author : "Фрэнсис Скотт Фицджеральд", nameBook : "Великий Гэтсби"),
+                new Book(Author : "Чак Паланик", nameBook : "Бойцовский клуб"),
+                new Book(Author : "Марио Пьюзо", nameBook : "Крестный отец")
+            };
 
         }
     }
